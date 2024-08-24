8b0d95e49d94b658b574956935bcc47f5cd9d22b
Ahmet Taha Aydemir <ahmetahaydemir@gmail.com>
2024-08-24 13:50:29 +0300

    ASN Server Basic

---
+++ server.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/netip"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/oschwald/maxminddb-golang/v2"
+)
+
+func main() {
+	app := fiber.New()
+	//
+	app.Get("/asn", func(c *fiber.Ctx) error {
+		db, err := maxminddb.Open("asn.mmdb")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+		//
+		query := c.IP()
+		addr, addErr := netip.ParseAddr(query)
+		if addErr != nil {
+			return c.SendString("Invalid Protocol - 1")
+			// panic(addErr)
+		}
+		//
+		var record struct {
+			Domain string `maxminddb:"domain"`
+			ASN    string `maxminddb:"asn"`
+			Name   string `maxminddb:"name"`
+		}
+		err = db.Lookup(addr).Decode(&record)
+		if err != nil {
+			return c.SendString("Invalid Structure - 2")
+			// log.Panic(err)
+		}
+		//
+		if record.ASN == "" {
+			return c.SendString("Invalid Lookup - 3")
+			// log.Panic(err)
+		}
+		fmt.Println(record.ASN)
+		fmt.Println(record.Domain)
+		fmt.Println(record.Name)
+		//
+		return c.SendString(c.IP() + "|" + record.ASN + "|" + record.Name + "|" + record.Domain)
+	})
+	app.Get("/asn/:value", func(c *fiber.Ctx) error {
+		db, err := maxminddb.Open("asn.mmdb")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
+		//
+		query := c.Params("value")
+		addr, addErr := netip.ParseAddr(query)
+		if addErr != nil {
+			return c.SendString("Invalid Protocol - 1")
+			// panic(addErr)
+		}
+		//
+		var record struct {
+			Domain string `maxminddb:"domain"`
+			ASN    string `maxminddb:"asn"`
+			Name   string `maxminddb:"name"`
+		}
+		err = db.Lookup(addr).Decode(&record)
+		if err != nil {
+			return c.SendString("Invalid Structure - 2")
+			// log.Panic(err)
+		}
+		//
+		if record.ASN == "" {
+			return c.SendString("Invalid Lookup - 3")
+			// log.Panic(err)
+		}
+		fmt.Println(record.ASN)
+		fmt.Println(record.Domain)
+		fmt.Println(record.Name)
+		//
+		return c.SendString(query + "|" + record.ASN + "|" + record.Name + "|" + record.Domain)
+	})
+	//
+	app.Listen(":3000")
+}
